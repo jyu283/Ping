10a5fbf06833db2ce9c31fea7777b4984c470174
Jerry Yu <jyu283@wisc.edu>
2020-04-21 13:35:18 -0500

    did more stuff.

--- src/ping.c
+++ src/ping.c
@@ -1,30 +1,49 @@
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <netinet/ip_icmp.h>
 
 #include <netdb.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <time.h>
 
-struct dns_result {
-    char host[256];
-    char addr[64];
+#define DEFAULT_TTL         64
+#define PING_PACKET_SIZE    64
+#define RECV_TIMEOUT        1
+#define SLEEP_SEC           1
+
+struct ping_packet {
+    struct icmphdr hdr;
+    char msg[PING_PACKET_SIZE - sizeof(struct icmphdr)];
 };
 
-static inline void print_prompt(void)
-{
-    printf("ping> ");
-    fflush(stdout);
-}
+static int ping_continue = 1;
 
 static inline void print_help(void)
 {
-    printf("Usage: hostname/IP\n");
-    fflush(stdout);
+    puts("Usage: hostname/IP");
 }
 
+unsigned short checksum(void *b, int len) 
+{    
+	unsigned short *buf = b; 
+    unsigned int sum=0; 
+    unsigned short result; 
+  
+    for ( sum = 0; len > 1; len -= 2 ) 
+        sum += *buf++; 
+    if ( len == 1 ) 
+        sum += *(unsigned char*)buf; 
+    sum = (sum >> 16) + (sum & 0xFFFF); 
+    sum += (sum >> 16); 
+    result = ~sum; 
+    return result; 
+} 
+
 /* Perform a DNS lookup on a given hostname. */
-static struct dns_result *dns_lookup(char *hostname)
+static struct addrinfo *dns_lookup(char *hostname)
 {
     int status;
     struct addrinfo hints;
@@ -40,71 +59,156 @@ static struct dns_result *dns_lookup(char *hostname)
         return NULL;
     }
 
-    struct dns_result *result = malloc(sizeof(struct dns_result));
-    struct addrinfo *info;
-    for (info = servinfo; info != NULL; info = info->ai_next) {
-        getnameinfo(info->ai_addr, info->ai_addrlen, 
-                    result->host, sizeof(result->host),
-                    NULL, 0, NI_DGRAM);
-        getnameinfo(info->ai_addr, info->ai_addrlen, 
-                    result->addr, sizeof(result->addr),
-                    NULL, 0, NI_NUMERICHOST);
+    if (servinfo) {
+        return servinfo;
+    } else {
+        return NULL;
     }
-    freeaddrinfo(info);
-    return result;
 } 
 
-/* Parse hostname from user input. */
-static int parse_input(char *input)
+static int init_socket(int *ttl, const char *timeout)
 {
-    char *hostname;
-
-    // Remove leading whitespace and trailing newline.
-    input[strlen(input)-1] = '\0';
-    if (!(hostname = strtok(input, "\t ")))
-        return 0;
-
-    // Built-in commands.
-    if (strcmp(hostname, "exit") == 0) {
+    struct protoent *proto = getprotobyname("ICMP");
+    int sockfd = socket(PF_INET, SOCK_RAW, proto->p_proto);
+    if (sockfd < 0) {
+        perror("socket");
         return -1;
-    } else if (strcmp(hostname, "help") == 0) {
-        print_help();
-        return 0;
     }
-
-    printf("Looking up hostname/IP: %s...\n", hostname);
-    struct dns_result *result = dns_lookup(hostname);
-    if (result) {
-        printf("%s\t%s\n", result->host, result->addr);
-        free(result);
+    if (setsockopt(sockfd, SOL_IP, IP_TTL, ttl, sizeof(int)) != 0) {
+        perror("TTL setting");
+        return -1;
     }
-    return 0;
+    setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO,
+               timeout, sizeof(struct timeval));
+    return sockfd;
 }
 
-/* Interactive Shell */
-static void cli(void)
+/* Initialize an empty ping packet */
+static void init_ping_packet(struct ping_packet *packet, int *seqno)
 {
-    int status;
-    char *line = NULL;
-    size_t len = 0;
-    ssize_t nread;
+    int i;
 
-    print_prompt();
-    while ((nread = getline(&line, &len, stdin)) != -1) {
-        if ((status = parse_input(line)) == -1){
-            break;
-        }
-        print_prompt();
+	// Zero out packet
+	bzero((void *)packet, sizeof(*packet));
+
+    packet->hdr.type = ICMP_ECHO;
+    packet->hdr.un.echo.id = getpid();
+
+    for (i = 0; i < sizeof(packet->msg) - 1; i++) {
+        packet->msg[i] = i + '0';
     }
-    free(line);
+    packet->msg[i] = 0;
+    packet->hdr.un.echo.sequence = ++*seqno;
+    packet->hdr.checksum = checksum(&packet, sizeof(*packet));
+}
+
+static inline int send_ping_packet(const int sockfd, struct ping_packet *packet, 
+                                   struct addrinfo *target)
+{
+    int bytes;
+    if ((bytes = sendto(sockfd, packet, sizeof(*packet), 0,
+                   target->ai_addr, target->ai_addrlen)) < 0) {
+        perror("Failed to send packet");
+        return 0;
+    } 
+    return 1;
+}
+
+static inline int recv_ping_packet(int sockfd, struct ping_packet *packet)
+{
+    int bytes;
+    struct sockaddr_in recv_addr;
+    socklen_t recv_addr_len = sizeof(recv_addr);
+
+    if ((bytes = recvfrom(sockfd, packet, sizeof(struct ping_packet), 0,
+                 (struct sockaddr *)&recv_addr, &recv_addr_len)) <= 0) {
+        perror("Failed to receive packet");
+        return -1;
+    }
+    return bytes;
+}
+
+/* Loop to perform ping. */
+static void do_ping(int sockfd, struct addrinfo *target, int *ttl)
+{
+    int seqno = 0, packet_sent, total_received = 0;
+    struct ping_packet packet;
+    struct timespec t_start, t_end;
+    double time_elapsed;
+    long double rtt_msec = 0;
+    char hostname[256];
+    char addr[64];
+
+    getnameinfo(target->ai_addr, target->ai_addrlen,
+                hostname, sizeof(hostname),
+                NULL, 0, NI_DGRAM);
+    getnameinfo(target->ai_addr, target->ai_addrlen,
+                addr, sizeof(addr),
+                NULL, 0, NI_NUMERICHOST);
+    if (target->ai_canonname) {
+        printf("PING %s (%s) %lu bytes of data\n", target->ai_canonname, 
+                addr, PING_PACKET_SIZE - sizeof(struct icmphdr));
+    } else {
+        printf("PING %s (%s) %lu bytes of data\n", hostname, addr, 
+                      PING_PACKET_SIZE - sizeof(struct icmphdr));
+    }
+    
+    // TODO: ping_continue will be set to 0 on SIGINT signal.
+    // while (ping_continue) {
+    for (int i = 0; i < 5; i++) {   // for loop for debugging ease
+        sleep(SLEEP_SEC);
+        init_ping_packet(&packet, &seqno);
+
+        // Send packet and start timer
+        clock_gettime(CLOCK_MONOTONIC, &t_start);
+        packet_sent = send_ping_packet(sockfd, &packet, target);
+
+        if (recv_ping_packet(sockfd, &packet)) {
+            clock_gettime(CLOCK_MONOTONIC, &t_end);
+            time_elapsed = ((double)(t_end.tv_nsec - t_start.tv_nsec))/1000000.0;
+            rtt_msec = (t_end.tv_sec - t_start.tv_sec) * 1000.0 + time_elapsed; 
+
+            if (!packet_sent) {
+                continue;
+            }
+
+            printf("%d bytes from %s (%s): icmp_seq=%d ttl=%d time=%Lf ms\n",
+                   PING_PACKET_SIZE, hostname, addr, seqno, *ttl, rtt_msec);
+            total_received++;  
+        }
+    }
+}
+
+/* Ping the given target. */
+static void ping(struct addrinfo *target)
+{
+    int sockfd, ttl = 255;
+    struct timeval tv_out;
+    tv_out.tv_sec = RECV_TIMEOUT;
+    tv_out.tv_usec = 0;
+
+    sockfd = init_socket(&ttl, (const char *)&tv_out);
+    if (sockfd == -1)
+        return;
+
+    do_ping(sockfd, target, &ttl);
+
+    freeaddrinfo(target);
+    target = NULL;
 }
 
 int main(int argc, char *argv[])
 {
-    printf("PING v0.1-4.20.20\n");
-    printf("Copyright (c) 2020 Jerry Yu.\n");
-    printf("Type \"help\" for more information. ");
-    printf("Type \"exit\" to quit the program.\n");
-    cli();
-    printf("Better hire Jerry!\n");
+    if (argc == 1) {
+        print_help();
+        exit(0);
+    }
+
+    struct addrinfo *target = dns_lookup(argv[1]);
+    if (!target)
+        return 0;
+    
+    ping(target);
+
+    return 0;
 }
